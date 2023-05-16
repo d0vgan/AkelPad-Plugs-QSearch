4db2fdd21fbbf9ff9f08ca98d54979ce5eb47933
d0vgan <dvv81@ukr.net>
2023-05-16 12:57:09 +0300

    adding Build_VC2022.cmd and QSearch2022.sln

---
+++ Source/Build_VC2022.cmd
@@ -0,0 +1,46 @@
+REM Visual Studio 2022 Community or Professional
+
+set VC_ROOT=%ProgramFiles%\Microsoft Visual Studio\2022
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
+set MSSDK=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A
+set PATH=%MSSDK%\bin;%PATH%
+
+cd "%~dp0."
+if not exist "..\Plugs" mkdir "..\Plugs"
+cd .\QSearch
+
+rc /r /Fo"QSearch.res" "QSearch.rc"
+cl /O1 /GS- QSearch.c QSearchDlg.c QSearchSettDlg.c QSearchLng.c QSearchFindEx.c DialogSwitcher.c XMemStrFunc.c QSearch.res /LD /link kernel32.lib user32.lib comctl32.lib gdi32.lib Advapi32.lib /NODEFAULTLIB /ENTRY:DllMain /OUT:..\..\Plugs\QSearch.dll
+
+if exist QSearch.res del QSearch.res
+if exist QSearch.lib del QSearch.lib
+if exist QSearch.exp del QSearch.exp
+if exist QSearch.obj del QSearch.obj
+if exist QSearchDlg.obj del QSearchDlg.obj
+if exist QSearchSettDlg.obj del QSearchSettDlg.obj
+if exist QSearchLng.obj del QSearchLng.obj
+if exist QSearchFindEx.obj del QSearchFindEx.obj
+if exist DialogSwitcher.obj del DialogSwitcher.obj
+if exist XMemStrFunc.obj del XMemStrFunc.obj
+@PAUSE
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
